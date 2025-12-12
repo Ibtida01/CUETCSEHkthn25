8975c0808206ac78da501fb6d78b7d85c55040d5
Ibtida01 <ibtida01.ahmed@gmail.com>
2025-12-12 17:45:08 +0600

    fix: allow console.log in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,45 @@
-import baseConfig from "@hono/eslint-config";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...baseConfig, {
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
-    },
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'build/**',
+      '.github/**',
+      'docker/**',
+      '*.config.js',
+      '*.config.mjs'
+    ]
   },
-});
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      // ALLOW ALL CONSOLE METHODS
+      'no-console': 'off',
+      'no-debugger': 'warn',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off'
+    }
+  }
+];
